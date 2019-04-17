556dc470230fc882f0ba624ef5db199e2ac8d081
khlokesh-184 <khlokesh184@gmail.com>
2019-04-18 04:07:36 +0530

    lk

--- tmp.py
+++ tmp.py
@@ -210,7 +210,7 @@ if __name__ == "__main__":
             model.fit(
                 train_features,
                 train_labels,
-                epochs=3,
+                epochs=5,
                 callbacks=[checkpoint, earlystop],
                 verbose=1,
                 validation_data=(test_features, test_labels),
